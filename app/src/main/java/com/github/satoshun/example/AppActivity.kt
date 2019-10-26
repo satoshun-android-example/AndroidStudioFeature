9d76de73d8b0138b6028402c288ed9549504d57d
SatoShun <shun.sato1@gmail.com>
2019-10-26 00:48:00 +0000

    first

---
+++ app/src/main/java/com/github/satoshun/example/AppActivity.kt
@@ -0,0 +1,33 @@
+package com.github.satoshun.example
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.github.satoshun.example.databinding.AppActBinding
+import com.github.satoshun.example.databinding.AppFragBinding
+
+class AppActivity : AppCompatActivity() {
+  private lateinit var binding: AppActBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = DataBindingUtil.setContentView(this, R.layout.app_act)
+  }
+}
+
+class AppFragment : Fragment() {
+  private lateinit var binding: AppFragBinding
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    binding = AppFragBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+  }
+}
